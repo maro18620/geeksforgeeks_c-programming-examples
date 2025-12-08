f4a7afe00513725895756e7f45cf14eba33b870b
marwanmohamed213410 <marwanmohamed2238@gmail.com>
2025-12-08 21:52:14 +0200

    make betty happy

--- Basic/1-Print_Your_Own_Name.c
+++ Basic/1-Print_Your_Own_Name.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
-
+/**
+ * main - entry point
+ *
+ * Description: print your own name
+ *
+ * Return: 0 (success)
+*/
 int main(void)
 {
-    char name[10] = "unknown";
-    printf("Enter your name: ");
-    scanf(" %s", name);
-    printf("your name is: %s\n", name);
+	char name[10] = "unknown";
+
+	printf("Enter your name: ");
+	scanf(" %s", name);
+	printf("your name is: %s\n", name);
+	return (0);
 }
